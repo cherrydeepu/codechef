395b4a6d4029a15f7002890dc06809e3b479f95e
cherrydeepu <trishithapandu2218@gmail.com>
2024-12-06 11:54:32 +0530

    Add files via upload

---
+++ Biryani.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Biryani
+{
+	public static void main (String[] args) 
+	{
+		Scanner s=new Scanner(System.in);
+		int t=s.nextInt();
+		for(int i=0;i<=t;i++)
+		{
+		int x=s.nextInt();
+		int y=s.nextInt();
+		int a=x*y;
+		System.out.println(a);
+		}
+	}
+}
