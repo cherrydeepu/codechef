1c8015d2fac0ce2763fc004261b063f441eec9d8
cherrydeepu <trishithapandu2218@gmail.com>
2024-12-06 11:11:24 +0530

    Add files via upload

---
+++ YogaDay.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class YogaDay
+{
+	public static void main (String[] args) 
+	{
+	    Scanner s=new Scanner(System.in);
+	    int a=s.nextInt();
+	    int b=a/12;
+	    if(b!=0)
+	    {
+	     System.out.println(b); 
+	    }
+
+	}
+}
