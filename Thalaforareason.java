7f4534db4f3c286a4cd6a5887f079d434d027467
cherrydeepu <trishithapandu2218@gmail.com>
2024-12-09 06:53:24 +0530

    Add files via upload

---
+++ Thalaforareason.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Thalaforareason{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int N=s.nextInt();
+        if(N==7)
+        {
+            System.out.println("THALA");    
+        }else{
+            System.out.println("SADGE");
+        }
+        
+    }
+}
