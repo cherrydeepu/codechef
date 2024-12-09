8268c49f8002dc8f957aaeb39bf122027bee34ad
cherrydeepu <trishithapandu2218@gmail.com>
2024-12-09 06:52:58 +0530

    Add files via upload

---
+++ Moneymaking.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Moneymaking{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int X=sc.nextInt();
+        int Y=sc.nextInt();
+        int c=5000;
+        int d=9800;
+        X=X*c;
+        Y=Y*d;
+        int Z=X+Y;
+        System.out.println(Z);
+    }
+}
