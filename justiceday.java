e5a14755f34bd5f6307a4552eeaacaf076bdf2fc
cherrydeepu <trishithapandu2218@gmail.com>
2024-12-09 10:27:45 +0530

    Add files via upload

---
+++ justiceday.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class justiceday{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int X=s.nextInt();
+        int Y=s.nextInt();
+        if(X>=Y){
+            System.out.println("YES");
+        }else{
+            System.out.println("NO");
+        }
+    }
+}
