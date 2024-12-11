2e4dae32341a70cab259bb2a8c6f4b6a382f10ca
Rishitha <trishithapandu2218@gmail.com>
2024-12-11 20:51:49 +0530

    Add files via upload

---
+++ CodechefRound.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Codechef{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int N=s.nextInt();
+        if(N==4)
+        {
+            System.out.println("YES");
+        }else{
+            System.out.println("NO");
+        }
+    }
+}
