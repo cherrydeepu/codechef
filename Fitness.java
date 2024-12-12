d5ae94c7ac02c7f6e9e29e469401b3c39b04db76
Rishitha <trishithapandu2218@gmail.com>
2024-12-12 20:13:59 +0530

    Add files via upload

---
+++ Fitness.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Codechef {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int T= scanner.nextInt();
+        for(int i=0;i<T;i++){
+            int X=scanner.nextInt();
+            System.out.println(X*10);
+        }
+    }
+}
