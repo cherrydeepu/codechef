0e3815bf7fe89fa7d2c83cf498cfc21017db88e1
Rishitha <trishithapandu2218@gmail.com>
2024-12-16 21:39:32 +0530

    Add files via upload

---
+++ MorningRun.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+        Scanner s=new Scanner(System.in);
+        int X=s.nextInt();
+        int Y=s.nextInt();
+        if ((X+Y)>=500){
+            System.out.println("YES");
+        }else{
+            System.out.println("NO");
+        }
+	}
+}
