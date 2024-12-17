d75f6beaf38a9548c991a3530702eb85c2386f4e
Rishitha <trishithapandu2218@gmail.com>
2024-12-17 21:04:14 +0530

    Star01.java

---
+++ Star01.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class Codechef{
+    public static void main(String args[]){
+        Scanner n=new Scanner(System.in);
+        int X=n.nextInt();
+        System.out.println(X);
+    }
+}
