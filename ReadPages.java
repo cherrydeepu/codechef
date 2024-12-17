dcfdca1bb5c14aa7cc266b004475ab0ee6158907
Rishitha <trishithapandu2218@gmail.com>
2024-12-17 21:03:39 +0530

    ReadPages.java

---
+++ ReadPages.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Codechef{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int T=s.nextInt();
+        for(int i=0;i<T;i++)
+        {
+            int N=s.nextInt();
+            int X=s.nextInt();
+            int Y=s.nextInt();
+            if(X*Y>=N )
+            {
+                System.out.println("YES");
+            }
+            else
+            {
+                System.out.println("NO");
+            }
+        }
+    }
+}
