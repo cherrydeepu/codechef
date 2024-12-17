b42bdd6f239a16dd0355a0674578e1b86a77aed8
Rishitha <trishithapandu2218@gmail.com>
2024-12-17 20:55:44 +0530

    CClearningPS.java

---
+++ CClearningPS.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Codechef
+{
+	public static void main (String[] args) 
+    {
+        Scanner s=new Scanner(System.in);
+        int N=s.nextInt();
+        System.out.println(N*2);
+	}
+}
