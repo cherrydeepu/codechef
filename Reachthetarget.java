de7b7e33b80e44dfd801ecb29fe37abeb246c254
Rishitha <trishithapandu2218@gmail.com>
2024-12-17 21:02:48 +0530

    Reachthetarget.java

---
+++ Reachthetarget.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class codechef{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int T=s.nextInt();
+        for(int i=0;i<T;i++){
+            int X=s.nextInt();
+            int Y=s.nextInt();
+            System.out.println(X-Y);
+        }
+    }
+}
