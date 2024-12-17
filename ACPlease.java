f7adfc97285e54ef77dc78ff548d9730144ae973
Rishitha <trishithapandu2218@gmail.com>
2024-12-17 20:39:26 +0530

    ACPlease.java

---
+++ ACPlease.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+        Scanner s=new Scanner(System.in);
+        int T=s.nextInt();
+        if(T>30){
+            System.out.println("YES");
+        }else{
+            System.out.println("NO");
+        }
+	}
+}
