4a309bb02e3a0d5da97bf7bbef563a95b67fb155
Rishitha <trishithapandu2218@gmail.com>
2024-12-19 20:24:40 +0530

    Add files via upload

---
+++ ChristmasCake.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class Codechef{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int X=s.nextInt();
+        System.out.println(25-X);
+    }
+}
