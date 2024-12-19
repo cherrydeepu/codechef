624f24d1551a62cf738b30f7703dbf9d09484fee
Rishitha <trishithapandu2218@gmail.com>
2024-12-19 20:21:29 +0530

    Add files via upload

---
+++ GlassPrice.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Codechef{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int X=s.nextInt();
+        int Y=s.nextInt();
+        if(2*X<Y){
+            System.out.println("PLASTIC");
+        }else{
+            System.out.println("METAL");
+        }
+    }
+}
