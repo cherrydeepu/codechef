0182e8480a18b489c3071133763b38fe110af430
Rishitha <trishithapandu2218@gmail.com>
2024-12-20 18:37:49 +0530

    Add files via upload

---
+++ OverSpeed.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Codechef{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int X=s.nextInt();
+        if(X<=40){
+            System.out.println("NO");
+        }else{
+            System.out.println("YES");
+        }
+    }
+}
