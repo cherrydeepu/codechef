00f4bd56cfd03e6a2bf6f868ec63a4b2348c67cf
Rishitha <trishithapandu2218@gmail.com>
2024-12-21 20:14:23 +0530

    Add files via upload

---
+++ EmailRemainder.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Codechef{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+            int U=s.nextInt();
+            int N=s.nextInt();
+            System.out.println(U-N);
+    }
+}
