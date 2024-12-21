ac9efb60bb386223e1691c7b13dce41643083610
Rishitha <trishithapandu2218@gmail.com>
2024-12-21 19:48:12 +0530

    Add files via upload

---
+++ CountingWords.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Codechef{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int T=s.nextInt();
+        for(int i=0;i<T;i++){
+            int N=s.nextInt();
+            int M=s.nextInt();
+            System.out.println(N*M);
+        }
+    }
+}
