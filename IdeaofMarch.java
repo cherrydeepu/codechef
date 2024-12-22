3948bf4349f171ffc8afbc2383643db11f015682
Rishitha <trishithapandu2218@gmail.com>
2024-12-22 11:30:47 +0530

    Add files via upload

---
+++ IdeaofMarch.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Codechef{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int N=s.nextInt();
+        if(N==15){
+            System.out.println("YES");
+        }else{
+            System.out.println("NO");
+        }
+    }
+}
