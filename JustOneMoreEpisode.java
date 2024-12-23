d390b1b4dd6eec27f9f419cb63b919aa841c226f
Rishitha <trishithapandu2218@gmail.com>
2024-12-23 22:16:26 +0530

    Add files via upload

---
+++ JustOneMoreEpisode.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Codechef{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int T=s.nextInt();
+        for(int i=0;i<T;i++){
+            int X=s.nextInt();
+            if(X>24){
+                System.out.println("YES");
+            }else{
+                System.out.println("NO");
+            }
+        }
+    }
+}
