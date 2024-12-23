55bd74e5eacdc8d5c96d29adcab8b620fde76da9
Rishitha <trishithapandu2218@gmail.com>
2024-12-23 22:09:59 +0530

    Add files via upload

---
+++ RemainingNeighbourhood.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class Codechef{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int N=s.nextInt();
+        System.out.println(100-N);
+    }
+}
