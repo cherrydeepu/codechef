1753b2d147c58c91c7fe95a3271258f3a46b4935
Rishitha <trishithapandu2218@gmail.com>
2024-12-23 22:04:59 +0530

    Add files via upload

---
+++ OctoberMarathon.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Codechef{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int X=s.nextInt();
+        if(X<3){
+            System.out.println("GOLD");
+        }else if(X>=3 && X<6 ){
+            System.out.println("SILVER");
+        }else{
+            System.out.println("BRONZE");
+        }
+    }
+}
