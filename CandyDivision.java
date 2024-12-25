7b42fd5ae9c53ff09c1560f3b2b13566d3ea8a86
Rishitha <trishithapandu2218@gmail.com>
2024-12-25 22:55:28 +0530

    Add files via upload

---
+++ CandyDivision.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+        Scanner s=new Scanner(System.in);
+        int T=s.nextInt();
+        for(int i=0;i<T;i++){
+            int N=s.nextInt();
+            if(N%3==0){
+                System.out.println("YES");
+            }else{
+                System.out.println("NO");
+            }
+        }
+	}
+}
