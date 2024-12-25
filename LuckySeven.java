8219c1efa442fd02f823c8d71323484426303891
Rishitha <trishithapandu2218@gmail.com>
2024-12-25 22:56:11 +0530

    Add files via upload

---
+++ LuckySeven.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) 
+	{
+        Scanner s=new Scanner(System.in);
+        String Sc=s.next();
+        System.out.println(Sc.charAt(6));
+	}
+}
