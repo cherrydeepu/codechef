9054b92a7e20724c863b137295249586ddb4f0f0
Rishitha <trishithapandu2218@gmail.com>
2024-12-26 19:37:54 +0530

    Add files via upload

---
+++ LuckyClover.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Codechef{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int N=s.nextInt();
+        N=N-1;
+        System.out.println(4+(3*N));
+    }
+}
