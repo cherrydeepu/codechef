18be9b31697c0dcfe049b83b466522cda191b87d
Rishitha <trishithapandu2218@gmail.com>
2024-12-27 09:48:03 +0530

    Add files via upload

---
+++ UnattemptedProblems.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Codechef{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int X=s.nextInt();
+        int Y=s.nextInt();
+        System.out.println(X-Y);
+    }
+}
