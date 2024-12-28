2177de2adfc20211d2aa5d77d7d67f8502bf1955
Rishitha <trishithapandu2218@gmail.com>
2024-12-28 19:59:13 +0530

    Add files via upload

---
+++ PuzzleHunt.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Codechef{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int N=s.nextInt();
+        if(N==6 ||N==7 || N==8){
+            System.out.println("YES");
+        }else{
+            System.out.println("NO");
+        }
+    }
+}
