5407026a9d1d1b75ea85cd5aba56da9a2c21d40d
Rishitha <trishithapandu2218@gmail.com>
2024-12-29 10:07:27 +0530

    Add files via upload

---
+++ TotalPrizeMoney.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Codechef{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int T=s.nextInt();
+        for(int i=0;i<T;i++){
+            int X=s.nextInt();
+            int Y=s.nextInt();
+            System.out.println(10*X+90*Y);
+        }
+    }
+}
