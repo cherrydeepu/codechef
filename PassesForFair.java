a816f8e0fe38280e56f4fedfc8d7842125fc969c
Rishitha <trishithapandu2218@gmail.com>
2025-01-01 21:57:36 +0530

    Add files via upload

---
+++ PassesForFair.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Codechef{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int T=s.nextInt();
+        for(int i=0;i<T;i++){
+            int N=s.nextInt();
+            int K=s.nextInt();
+            if(N<K){
+                System.out.println("YES");
+            }else{
+                System.out.println("NO");
+            }
+        }
+    }
+}
