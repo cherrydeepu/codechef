b0ef147e5eb20c730484e13467b69187d9d864a3
Rishitha <trishithapandu2218@gmail.com>
2025-01-01 21:59:05 +0530

    Add files via upload

---
+++ CoupleGame.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Codechef{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int T=s.nextInt();
+        for(int i=0;i<T;i++){
+            int G=s.nextInt();
+            int B=s.nextInt();
+            System.out.println(B-G);
+        }
+    }
+}
