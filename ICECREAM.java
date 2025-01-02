fbaf6a77e307244312352bf4ff7d1d844d709cd1
Rishitha <trishithapandu2218@gmail.com>
2025-01-02 19:31:51 +0530

    Add files via upload

---
+++ ICECREAM.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Codechef{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int X=s.nextInt();
+        int Y=s.nextInt();
+        X=X*2;
+        if(Y>=X){
+            System.out.println("YES");
+        }else{
+            System.out.println("NO");
+        }
+    }
+} 
