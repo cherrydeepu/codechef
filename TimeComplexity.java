93601a4cfed6b911c5d5e89dc809d16fcbb28fb9
Rishitha <trishithapandu2218@gmail.com>
2025-01-03 21:13:23 +0530

    Add files via upload

---
+++ TimeComplexity.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Codechef{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int T=s.nextInt();
+        for(int i=0;i<T;i++)
+        {
+            int X=s.nextInt();
+            int Y=s.nextInt();
+            if(X>Y){
+                System.out.println("YES");
+            }else{
+                System.out.println("NO");
+            }
+        }
+    }
+}
