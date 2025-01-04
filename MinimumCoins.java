5317e7bba3cef22fae893be20d68ed7b7cf06d74
Rishitha <trishithapandu2218@gmail.com>
2025-01-04 16:30:18 +0530

    Add files via upload

---
+++ MinimumCoins.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Codechef{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int T=s.nextInt();
+        for(int i=0;i<T;i++){
+            int X=s.nextInt();
+            System.out.println(X%10);
+        }
+    }
+}
