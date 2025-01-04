66bd8a947d83b7a21975603ced368d2baa43021d
Rishitha <trishithapandu2218@gmail.com>
2025-01-04 16:29:34 +0530

    Add files via upload

---
+++ WorthofaVideo.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+        Scanner sc=new Scanner(System.in);
+        int T=sc.nextInt();
+        for(int i=0;i<T;i++){
+            int s=sc.nextInt();
+            System.out.println(s*24*1000);
+        }
+	}
+}
