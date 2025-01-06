af0fc5e8862ea2dd90856cc092654dfdc0f6d92e
Rishitha <trishithapandu2218@gmail.com>
2025-01-06 18:58:12 +0530

    Add files via upload

---
+++ HeightofRationals.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Codechef{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        int b=s.nextInt();
+        if(a/b==0){
+            System.out.println(b);
+        }else{
+            System.out.println(a);
+        }
+    }
+}
