6abddebd2e7d05064f7ded703c176b46820d98c3
Rishitha <trishithapandu2218@gmail.com>
2025-01-13 10:18:05 +0530

    Add files via upload

---
+++ AliceMarks.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int X = sc.nextInt();
+		int Y = sc.nextInt();
+		if(X>=2*Y){
+		    System.out.println("Yes");
+		}else{
+		    System.out.println("No");
+		}
+	}
+}
