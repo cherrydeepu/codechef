844a2a779ba9446befb1bfd776b444074843b118
Rishitha <trishithapandu2218@gmail.com>
2025-01-13 10:17:16 +0530

    Add files via upload

---
+++ AppleOrange.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner s=new Scanner(System.in);
+	    int X=s.nextInt();
+	    int A=s.nextInt();
+	    int B=s.nextInt();
+	    if((A+B)<=X){
+	        System.out.println("Yes");
+	    }else{
+	        System.out.println("No");
+	    }
+	}
+}
