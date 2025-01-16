5c50b1b4f877743c9963752ef6fa46edacf8e0ea
Rishitha <trishithapandu2218@gmail.com>
2025-01-16 09:42:24 +0530

    Add files via upload

---
+++ WaterPark.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+        Scanner s=new Scanner(System.in);
+        int W=s.nextInt();
+        int H=s.nextInt();
+        if(W>=60 && H<=130){
+            System.out.println("YES");
+        }else{
+            System.out.println("NO");
+        }
+	}
+}
