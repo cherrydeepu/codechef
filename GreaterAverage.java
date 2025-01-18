2f495bad24b7e2dfe8489f618dad6cd0a262c161
Rishitha <trishithapandu2218@gmail.com>
2025-01-18 10:23:55 +0530

    Add files via upload

---
+++ GreaterAverage.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+        Scanner s=new Scanner(System.in);
+        int T=s.nextInt();
+        for(int i=0;i<T;i++){
+            int A=s.nextInt();
+            int B=s.nextInt();
+            int C=s.nextInt();
+            if((A+B)/2.0>C){
+                System.out.println("YES");
+            }else{
+                System.out.println("NO");
+            }
+        }
+	}
+}
