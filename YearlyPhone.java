fe048a1a38d520e4a13019e7148f5d1e58285274
Rishitha <trishithapandu2218@gmail.com>
2025-01-22 18:48:30 +0530

    Add files via upload

---
+++ YearlyPhone.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+        Scanner s=new Scanner(System.in);
+        int X=s.nextInt();
+        X=X%100;
+        System.out.printf("K%02d\n",X%100);
+	}
+}
