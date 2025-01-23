bd79e1e546d7130a4754ee53cd0d8621c913e1e3
Rishitha <trishithapandu2218@gmail.com>
2025-01-23 20:58:43 +0530

    Add files via upload

---
+++ CountTheNotebooks.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+        Scanner s=new Scanner(System.in);
+        int T=s.nextInt();
+        for(int i=0;i<T;i++){
+            int N=s.nextInt();
+            System.out.println(N*10);
+        }
+	}
+}
