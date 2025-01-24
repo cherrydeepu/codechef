3a114c4c554934251e2094fdbe99cb77610edea1
Rishitha <trishithapandu2218@gmail.com>
2025-01-24 22:10:50 +0530

    Add files via upload

---
+++ SugarcaneJuiceBusiness.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+        Scanner s=new Scanner(System.in);
+        int T=s.nextInt();
+        for(int i=0;i<T;i++){
+            int N=s.nextInt();
+            N=N*50-(N*50*20/100)-(N*50*20/100)-(N*50*30/100);
+            System.out.println(N);
+        }
+	}
+}
