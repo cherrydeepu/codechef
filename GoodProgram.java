6439035bb50192b58164476a4909e4077cf9942f
Rishitha <trishithapandu2218@gmail.com>
2025-01-26 17:06:34 +0530

    Add files via upload

---
+++ GoodProgram.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+        Scanner s=new Scanner(System.in);
+        int T=s.nextInt();
+        for(int i=0;i<T;i++){
+            int N=s.nextInt();
+            if(N%4==0){
+                System.out.println("Good");
+            }else{
+                System.out.println("Not Good");
+            }
+        }
+	}
+}
