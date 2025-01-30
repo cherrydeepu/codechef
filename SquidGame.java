d0f728ec750568a025651b57ab30baac23d36949
Rishitha <trishithapandu2218@gmail.com>
2025-01-30 23:20:21 +0530

    Add files via upload

---
+++ SquidGame.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+        Scanner s=new Scanner(System.in);
+        int K=s.nextInt();
+        int N=s.nextInt();
+        System.out.println((K-N)*10000);
+	}
+}
+
