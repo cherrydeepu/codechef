c560d922e5f81ccc8fc4903c3cf210f1da80018f
Rishitha <trishithapandu2218@gmail.com>
2025-04-28 20:09:57 +0530

    Add files via upload

---
+++ IPL.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class IPL
+{
+	public static void main (String[] args) 
+	{
+        Scanner s=new Scanner(System.in);
+        int X=s.nextInt();
+        if(X>=7)
+        {
+            System.out.println("THALA");
+        }
+        else
+        {
+            System.out.println("BOOM");
+        }
+	}
+}
