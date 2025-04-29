58bf9e993a5b8fa442f4f31618a51e9a3ecb21e2
Rishitha <trishithapandu2218@gmail.com>
2025-04-29 13:07:56 +0530

    Add files via upload

---
+++ TimeMachine.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class TimeMachine
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+        Scanner s=new Scanner(System.in);
+        int X=s.nextInt();
+        if(2050-X<=25)
+        {
+            System.out.println("Yes");
+        }
+        else
+        {
+            System.out.println("No");   
+        }
+	}
+}
