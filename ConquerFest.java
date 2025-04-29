86c39a715f8ed03d2003fc37b19fcf801e37e1c0
Rishitha <trishithapandu2218@gmail.com>
2025-04-29 12:49:37 +0530

    Add files via upload

---
+++ ConquerFest.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class ConquerFest
+{
+	public static void main (String[] args) 
+	{
+		Scanner s=new Scanner(System.in);
+		int N=s.nextInt();
+		int B=s.nextInt();
+		if(N>=B*10){
+		    System.out.println("YES");
+		}else{
+		    System.out.println("NO");   
+		}
+	}
+}
+ 
+ 
