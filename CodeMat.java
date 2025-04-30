1828ca3be66e5d48424ead4a8a8c7f61e7b59464
Rishitha <trishithapandu2218@gmail.com>
2025-04-30 15:53:25 +0530

    Add files via upload

---
+++ CodeMat.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class CodeMat
+{
+	public static void main (String[] args)
+	{
+	    Scanner s=new Scanner(System.in);
+	    int X=s.nextInt();
+	    int Y=s.nextInt();
+	    if(X<=Y){
+	        System.out.println("Yes");
+	    }
+	    else{
+	        System.out.println("No");
+	    }
+	}
+}
