1092b45409bae3f1bb8460d51a58d53b5491bcf6
Rishitha <trishithapandu2218@gmail.com>
2025-05-04 14:37:03 +0530

    Update and rename Mark.html to favouritenumbers.java

---
+++ favouritenumbers.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class FavouriteNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int T = scanner.nextInt(); // Number of test cases
+        
+        for (int i = 0; i < T; i++) {
+            int A = scanner.nextInt(); // The number to evaluate
+            
+            if (A % 2 == 0 && A % 7 == 0) {
+                System.out.println("Alice");
+            } else if (A % 2 == 1 && A % 9 == 0) {
+                System.out.println("Bob");
+            } else {
+                System.out.println("Charlie");
+            }
+        }
+
+        scanner.close();
+    }
+}
