97e83e0e0195c91c00c6777bdcd6c79b862b355e
Rishitha <trishithapandu2218@gmail.com>
2026-04-29 20:07:47 +0530

    Add files via upload

---
+++ Popcorn.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int X=s.nextInt();
+
+		    X=X-100;
+		    X=X/50;
+		    System.out.println(X);
+
+
+	}
+}
