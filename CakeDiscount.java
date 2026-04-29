b877a2a5f805ceba7b534ad846c12440628ccb25
Rishitha <trishithapandu2218@gmail.com>
2026-04-29 20:08:22 +0530

    Add files via upload

---
+++ CakeDiscount.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int N=s.nextInt();
+		if(N>=5){
+		    System.out.println(N*85);
+		}else{
+		    System.out.println(N*100);
+		}
+
+	}
+}
