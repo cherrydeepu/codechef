15fe1e7cec83d11bf3aeb33e38f70d0717acf251
Rishitha <trishithapandu2218@gmail.com>
2026-04-30 23:40:57 +0530

    Add files via upload

---
+++ ContestRegistration.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner s=new Scanner(System.in);
+		int X=s.nextInt();
+		int Y=s.nextInt();
+		if(Y<=X){
+		    System.out.println("0");
+		}else{
+		    System.out.println("100");
+		}
+
+	}
+}
