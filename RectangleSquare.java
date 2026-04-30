19fbb7fed4fc1d705d8896effcd6bc4e7a239ba0
Rishitha <trishithapandu2218@gmail.com>
2026-04-30 23:40:20 +0530

    Add files via upload

---
+++ RectangleSquare.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner s=new Scanner(System.in);
+	    int A=s.nextInt();
+	    int B=s.nextInt();
+	    int C=s.nextInt();
+	    
+        if(A*B==C*C){
+            System.out.println("Yes");
+        }else{
+            System.out.println("No");
+        }
+	}
+}
