a2f713fc563de43897761d5e67154ff34a5fc6a0
Rishitha <trishithapandu2218@gmail.com>
2026-05-22 21:48:45 +0530

    Add files via upload

---
+++ ChefBakesCake.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner s=new Scanner(System.in);
+	    int N=s.nextInt();
+	    int M=s.nextInt();
+	    System.out.println((M*50)-(N*30));
+
+	}
+}
