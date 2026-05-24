14e132c39dd89e99517686bef0a1d14625a7ba49
Rishitha <trishithapandu2218@gmail.com>
2026-05-24 21:11:39 +0530

    Add files via upload

---
+++ DicePlay.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int A=s.nextInt();
+		int B=s.nextInt();
+		if((A<=6) && (B<=6) && (A==B)){
+		    System.out.println("Yes");
+		}else{
+		    System.out.println("No");
+		}
+
+	}
+}
