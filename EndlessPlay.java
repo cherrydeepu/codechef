ded3bece564e5f58d952bca7ec97daa83107064a
Rishitha <trishithapandu2218@gmail.com>
2026-06-05 23:44:55 +0530

    Add files via upload

---
+++ EndlessPlay.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int X=s.nextInt();
+		int H=s.nextInt();
+		System.out.println((X-4)*24+H);
+
+	}
+}
