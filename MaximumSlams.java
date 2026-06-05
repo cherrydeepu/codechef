020f9159e81b1910008af9f583e683b5e1bafac9
Rishitha <trishithapandu2218@gmail.com>
2026-06-05 23:46:34 +0530

    Add files via upload

---
+++ MaximumSlams.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		int X=s.nextInt();
+		int years=(25-X+3)/4;
+		System.out.println(years);
+
+	}
+}
