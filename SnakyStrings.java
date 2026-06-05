1daeb4617c46e9f79c5d32d2e01358371f4a8da2
Rishitha <trishithapandu2218@gmail.com>
2026-06-05 23:45:55 +0530

    Add files via upload

---
+++ SnakyStrings.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner s=new Scanner(System.in);
+		String A=s.next();
+		if(A.charAt(0)=='s' || A.charAt(A.length()-1)=='s'){
+		    System.out.println("Yes");
+		}else{
+		    System.out.println("No");
+		}
+
+	}
+}
