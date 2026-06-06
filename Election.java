5b64de6e47d7b8118de84ad8db062e55c7ce176f
Rishitha <trishithapandu2218@gmail.com>
2026-06-06 23:11:21 +0530

    Add files via upload

---
+++ Election.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+		int N=s.nextInt();
+		int K=s.nextInt();
+		int majority=N/2+1;
+		System.out.println(Math.max(0,majority-K));
+
+	}
+}
