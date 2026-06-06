68e473167a24afb33bc201b23296531ef4b6907d
Rishitha <trishithapandu2218@gmail.com>
2026-06-06 23:11:53 +0530

    Add files via upload

---
+++ RushToExam.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+		int N=s.nextInt();
+		int M=s.nextInt();
+		int A=s.nextInt();
+		if(N*A>=M){
+		    System.out.println("Yes");
+		}else{
+		    System.out.println("No");
+		}
+
+	}
+}
