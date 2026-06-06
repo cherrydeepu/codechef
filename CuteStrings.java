4809ccbfae97e989c8f20c3e372f91165f7b4ae6
Rishitha <trishithapandu2218@gmail.com>
2026-06-06 23:13:14 +0530

    Add files via upload

---
+++ CuteStrings.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+		String str=s.next();
+		if(str.charAt(0)==str.charAt(2) && str.charAt(1)=='w'){
+		    System.out.println("Cute");
+		}else{
+		    System.out.println("No");
+		}
+
+	}
+}
