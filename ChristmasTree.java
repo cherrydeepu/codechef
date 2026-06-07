0da3900e2ee312efe7386720cb6fabbbdef67ca6
Rishitha <trishithapandu2218@gmail.com>
2026-06-07 21:59:27 +0530

    Add files via upload

---
+++ ChristmasTree.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+		int N=s.nextInt();
+		int A=s.nextInt();
+		int M=s.nextInt();
+		int B=s.nextInt();
+		System.out.println(M*B-N*A);
+
+	}
+}
