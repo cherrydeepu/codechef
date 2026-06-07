c039401c84d3273b1d7391416317ff964f8e42b8
Rishitha <trishithapandu2218@gmail.com>
2026-06-07 21:59:02 +0530

    Add files via upload

---
+++ CakeMaking.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+		int A=s.nextInt();
+		int B=s.nextInt();
+		System.out.println(A*B-Math.min(A,B));
+
+	}
+}
