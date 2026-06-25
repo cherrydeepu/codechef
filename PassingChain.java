96cc71352bd1d6584b5fa01e3b74facff9b7b5ad
Rishitha <trishithapandu2218@gmail.com>
2026-06-25 19:18:31 +0100

    Add files via upload

---
+++ PassingChain.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner s=new Scanner(System.in);
+		int T=s.nextInt();
+		while(T-->0){
+		    int N=s.nextInt();
+		    int K=s.nextInt();
+		    int ans=1+((N-s1)/K)*K;
+		    System.out.println(ans);
+		}
+
+	}
+}
