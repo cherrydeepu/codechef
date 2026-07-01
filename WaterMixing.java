ea418c078f0c99b118eac242df4e6d50e065f612
Rishitha <trishithapandu2218@gmail.com>
2026-07-01 15:06:42 +0100

    Add files via upload

---
+++ WaterMixing.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef {
+    public static void main(String[] args) throws java.lang.Exception {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+
+        while (T-- > 0) {
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            int X = sc.nextInt();
+            int Y = sc.nextInt();
+
+            if (B > A) {
+                if (B - A <= X)
+                    System.out.println("YES");
+                else
+                    System.out.println("NO");
+            } else if (B < A) {
+                if (A - B <= Y)
+                    System.out.println("YES");
+                else
+                    System.out.println("NO");
+            } else {
+                System.out.println("YES");
+            }
+        }
+
+        sc.close();
+    }
+}
